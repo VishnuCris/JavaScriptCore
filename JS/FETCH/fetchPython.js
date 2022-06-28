621598c704a3e69b379d7c025d7f4d31c769d260
vichuwindev <successivevichu717185@gmail.com>
2022-06-28 23:03:10 +0530

    access data from flask api

--- JS/FETCH/fetchPython.js
+++ JS/FETCH/fetchPython.js
@@ -1,8 +1,29 @@
-let pythonFetchBtn = document.getElementById('pythonFetchBtn');
-let myInit = {method : 'GET',header:{'content-type':'application/json','Accept': 'application/json'},mode:'cors',cache:'default'};
+const pythonFetchBtn = document.getElementById('pythonFetchBtn');
+const myInitGet = {method : 'GET',header:{'content-type':'application/json','Accept': 'application/json'},mode:'no-cors',cache:'default'};
+const myInitPost = {method : 'POST',header:{'content-type':'application/json','Accept': 'application/json'},mode:'no-cors',cache:'default'};
 pythonFetchBtn.addEventListener('click',()=>{
-	fetch('https://127.0.0.1:5000',{myInit})
+	fetch('http://127.0.0.1:5000/',{myInitGet})
 	.then((res) => res.json())
-	.then((resp) => console.log(resp))
+	.then(displayRsltInSpan)
 	.catch(err => console.log(err));
 })
+
+const displayRsltInSpan = (res,id="getPyData")=>{
+	console.log(res)
+	let getPyData = document.querySelector('#'+id);
+	getPyData.innerHTML = res.message;
+}
+
+const postPyBtn = document.getElementById('pyBtn');
+postPyBtn.addEventListener('click',()=>{
+	const textPy = document.querySelector('#textPy');
+	let data = textPy.value;
+	postPyData(data);
+})
+
+function postPyData(data){
+	fetch('http://127.0.0.1:5000/post',{myInitPost})
+	.then((res) => res.json())
+	.then(displayRsltInSpan)
+	.catch((err)=>console.log(err));
+}
